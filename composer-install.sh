79040d1a8fc229ec43c2306d94e57b9f5b0d89c3
Jared Chu <me@jaredchu.com>
2017-03-14 14:04:12 +0700

    Update composer-install.sh

--- composer-install.sh
+++ composer-install.sh
@@ -1,4 +1,5 @@
 cd ~
+y | sudo apt-get install php php-common php-mbstring php-dom
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
