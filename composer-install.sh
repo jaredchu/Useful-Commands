11a3dd1103bbbc8cf339c2bfd1611c84bdeb1d43
Jared Chu <me@jaredchu.com>
2017-03-14 20:31:46 +0700

    Update composer-install.sh

--- composer-install.sh
+++ composer-install.sh
@@ -1,11 +1,18 @@
 # script install composer automatically on UBUNTU
 cd ~
-sudo apt-get -y install php php-common php-mbstring php-dom
+if hash apt-get 2>/dev/null; then
+  sudo apt-get install -y php php-common php-mbstring php-dom
+else
+  sudo yum install -y php php-common php-mbstring php-dom
+fi
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 mkdir -p ~/bin/
 mv composer.phar ~/bin/composer
 chmod +x ~/bin/composer
-composer
-echo "script run successful"
+if hash composer 2>/dev/null; then
+  echo "script run successful!"
+else
+  echo "script run failed!"
+fi
