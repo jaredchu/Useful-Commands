bd81cefaa0a93d46ae8a7a289ea98230ce1b22c6
Jared Chu <4394552+jaredchu@users.noreply.github.com>
2021-01-25 19:54:07 +0700

    Update install-docker-ubuntu-20.sh

--- scripts/install-docker-ubuntu-20.sh
+++ scripts/install-docker-ubuntu-20.sh
@@ -2,14 +2,15 @@
 
 sudo apt-get update
 
-sudo apt install docker.io
+sudo apt install -f docker.io
 sudo systemctl enable --now docker
 sudo usermod -aG docker $USER
 newgrp docker
 
-sudo apt-get install curl
+sudo apt-get install -f curl
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 docker --version
 docker-compose version
+# END
