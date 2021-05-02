d65cad60fa535112ff7eb57095012b942ffa23a7
Jared Chu <4394552+jaredchu@users.noreply.github.com>
2021-05-02 17:41:35 +0700

    Update node-install.sh

--- scripts/node-install.sh
+++ scripts/node-install.sh
@@ -5,7 +5,7 @@ NODE_VERSION=v12.19.0
 ARCHIVE_NAME=node-$NODE_VERSION-linux-x64
 ARCHIVE_FILE=$ARCHIVE_NAME.tar.xz
  
-wget https://nodejs.org/dist/$NODE_VERSION/$ARCHIVE_FILE
+curl -O https://nodejs.org/dist/$NODE_VERSION/$ARCHIVE_FILE
 mv ~/bin/node-latest ~/bin/node-latest__$(date +%F-%T)
 tar xvfJ $ARCHIVE_FILE -C ~/bin/
 mv ~/bin/$ARCHIVE_NAME ~/bin/node-latest
